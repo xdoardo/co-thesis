bff80e01d60172969a4317b38098cf67df96554b
Edoardo Marangoni <ecmm@anche.no>
2023-02-17 10:30:24 +0100

    continue talk

--- talks/agda/build.sh
+++ talks/agda/build.sh
@@ -1,4 +1,4 @@
-xelatex -shell-escape deck.tex &&  
-xelatex -shell-escape deck.tex &&  
-xelatex -shell-escape deck.tex &&  
+xelatex -shell-escape -file-line-error deck.tex &&  
+xelatex -shell-escape -file-line-error deck.tex &&  
+xelatex -shell-escape -file-line-error deck.tex &&  
 rm -r deck.aux deck.fdb_latexmk deck.fls deck.listing deck.log deck.nav deck.out  deck.snm deck.toc deck.vrb  _minted-deck texput.log indent.log deck.xdv
